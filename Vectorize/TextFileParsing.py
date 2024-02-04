efebe9a9314d7650c49bf495e67c38470cbf025a
Bhavinrathava <bhavinr9@gmail.com>
2024-02-04 11:53:20 -0600

    Base Vector store enabled

--- Vectorize/TextFileParsing.py
+++ Vectorize/TextFileParsing.py
@@ -3,16 +3,12 @@ import PyPDF2
 
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+from sentence_transformers import SentenceTransformer
 
-from pymilvus import (
-    connections,
-    utility,
-    FieldSchema,
-    CollectionSchema,
-    DataType,
-    Collection,
-)
+from pymilvus import MilvusClient
 
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
 def read_data(folder_path):
     pdf_contents = []
@@ -33,8 +29,8 @@ def read_data(folder_path):
 def convert_to_chunks(data):
     character_splitter = RecursiveCharacterTextSplitter(
     separators=["\n\n", "\n", ". ", " ", ""],
-    chunk_size=1000,
-    chunk_overlap=0
+    chunk_size=256,
+    chunk_overlap=32
 )
     character_split_texts = character_splitter.split_text('\n\n'.join(data))
 
@@ -43,28 +39,46 @@ def convert_to_chunks(data):
 
     return character_split_texts
 
-
-
-    pass
-
 #Create a function to process the [[chunk1, chunk2, chunk3, ...], [chunk1, chunk2, chunk3, ...], ...] by removing stop words, punctuation, etc. from the chunks
 def process_chunks(data):
     return data
 
 #Create a function to convert the [[chunk1, chunk2, chunk3, ...], [chunk1, chunk2, chunk3, ...], ...] into list of [[embedding1, embedding2, embedding3, ...], [embedding1, embedding2, embedding3, ...], ...]
 def convert_to_embeddings(data):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
 
-    pass
+    embeddings = []
+    for chunk in data:
+        embeddings.append(model.encode(chunk))
+    print(len(embeddings[0]))
+    return embeddings
+
+def get_milvus_client(endpoint, key):
+    client = MilvusClient(uri=endpoint, token=key)
+
+    # # Create a collection
+    # client.create_collection(
+    #     collection_name=cfg.milvus.collection_name,
+    #     dimension=1536
+    # )
+
+    return client
 
 #Create a function to store the list of [[embedding1, embedding2, embedding3, ...], [embedding1, embedding2, embedding3, ...], ...] into a VDB
-def store_embeddings(data):
-    pass
+def store_embeddings(collection_name, client, chunks,embeddings):
+    data = []
+    #Prepare data for insertion
+    for (chunk, embedding) in zip(chunks, embeddings):
+        data.append({"text": chunk, "Vectorfield": embedding})
+    return client.insert(collection_name=collection_name, data=data)
 
-
-def main():
+@hydra.main(version_base=None, config_path="../config", config_name="milvus")
+def main(cfg):
     data = read_data("Data/")
     chunks = convert_to_chunks(data)
-    pass
+    embeddings = convert_to_embeddings(chunks)
+    client = get_milvus_client(cfg.milvus.endpoint, cfg.milvus.key)
+    store_embeddings(cfg.milvus.collection_name, client, chunks, embeddings)
 
 if __name__ == '__main__':
     main()
