a243d7017d337e2e9acfe85e91dce30c158a433d
Keyi Wang <114252105+stellawang196@users.noreply.github.com>
2024-02-22 23:05:33 -0600

    Update query_classify.py

--- RAG/query_classify.py
+++ RAG/query_classify.py
@@ -7,26 +7,22 @@ def load_model_tokenizer(model_path):
     return model, tokenizer
 
 def predict_query_label(query):
-    # Load model
+    # load model
     model, tokenizer = load_model_tokenizer('../Query_Classification/model_save/')
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Tokenize the input sentence
     inputs = tokenizer(query, padding=True, truncation=True, max_length=512)
-    
-    # Move input tensors to the device
     input_ids = inputs['input_ids'].to(device)
     attention_mask = inputs['attention_mask'].to(device)
     
-    # Perform inference
+    # perform inference
     with torch.no_grad():
         outputs = model(input_ids, attention_mask=attention_mask)
         logits = outputs.logits
     
-    # Convert logits to probabilities
     probabilities = torch.softmax(logits, dim=1)
     
-    # Get predicted label (assuming binary classification)
+    # get predicted label
     predicted_label = torch.argmax(probabilities, dim=1).item()
     
     return predicted_label
